b09cc112dc4f24558e9df88679150ea3bf7b88b7
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2021-04-28 21:45:38 +0700

    Update ONLINE_SCRIPT.lua

--- ONLINE_SCRIPT.lua
+++ ONLINE_SCRIPT.lua
@@ -52,10 +52,12 @@ end
 
 -- EVENT 
 
+
 function ANABELSCAN()
 local gg = gg
-	gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC | gg.REGION_JAVA_HEAP)
-gg.searchNumber('4D;22D;-199~-197;911~912;77~78::17', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+if io.open(VERSIENOL) ~= nil then
+	gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC | gg.REGION_JAVA_HEAP | gg.REGION_C_HEAP)
+gg.searchNumber('4D;-199~-197;911~912;77~78::17', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
 gg.refineNumber('-50000~50000', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
 gg.getResults(3)
 t = gg.getResults(3)
@@ -65,7 +67,20 @@ r[2].value = -4771
 r[3].value = 94
 gg.setValues(r)
 gg.clearResults()
-EVENTLBBWT()
+elseif io.open(VERSISATU) ~= nil then
+	gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC)
+gg.searchNumber('4D;-199~-197;911~912;77~78::17', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('-50000~50000', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3)
+t = gg.getResults(3)
+local r = gg.getResults(3)
+r[1].value = 2495
+r[2].value = -4771
+r[3].value = 94
+gg.setValues(r)
+gg.clearResults()
+else gg.alert('OPERATION FAILED. NO DATA / NEED UPDATE') 
+end
 end
 
 
@@ -80,7 +95,9 @@ elseif v.name == 'SPD'	then v.value = 1
 end
 end
 gg.setValues(t)
+ANABELSCAN()
 end ---------------------------
+
 
 
 function EVENTLBROCKBITTER()
@@ -88,7 +105,7 @@ t = gg.getListItems()
 for i, v in ipairs(t) do
 ---------------------------------------------
 	if v.name == 'UD' and v.value == 1214 -- BWT
-	then ANABELSCAN()
+	then EVENTLBBWT()
 ---------------------------------------------
 	elseif v.name == 'UD' and v.value == -128 -- MAP AWAL
 	then EVENTLBROCKBITTERA()
